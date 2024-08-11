0f3c10d07ff8b998192184e2ce21161c79e60b07
Apurva Sadashiv Patil <161266535+ApurvaPatil2401@users.noreply.github.com>
2024-08-11 16:50:55 +0530

    server.py

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
 
+from flask import Flask, request, jsonify
+from emotion_detector import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/emotion', methods=['POST'])
+def analyze_emotion():
+    text_to_analyze = request.get_json()['text']
+    result = emotion_detector(text_to_analyze)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
